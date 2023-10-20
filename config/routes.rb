70a325f5022429e341fb2a7f5e42d15d8ae782d9
Clarence Atim <125105700+ClarenceAtim@users.noreply.github.com>
2023-10-21 03:11:19 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,24 +7,8 @@ Rails.application.routes.draw do
     resources :recipe_foods, only: [:new, :create, :show, :destroy]
   end
 
-  resources :foods, only: [:index, :new, :create, :show, :destroy]
+  resources :foods, only: [:index, :new, :create, :destroy]
   resources :shopping_lists, only: [:index]
 
   root 'recipes#index'
 end
-
-
-
-
-
-# Rails.application.routes.draw do
-#   devise_for :users, path: 'auth',
-#   path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret',
-#    confirmation: 'verification', unlock: 'unblock', registration: 'register',
-#    sign_up: 'cmon_let_me_in' } 
-#    devise_scope :user do
-#     get '/users/sign_out', to: 'devise/sessions#destroy'
-#     root to: 'devise/sessions#new'
-#   end
-#     # root 'users#index'
-# end
