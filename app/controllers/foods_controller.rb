facee6a7944673353fcaa58907ee71c4c65ffcb4
Clarence Atim <125105700+ClarenceAtim@users.noreply.github.com>
2023-10-20 17:17:02 +0530

    Update foods_controller.rb

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,32 +1,32 @@
-# class FoodsController < ApplicationController
-#   before_action :authenticate_user!
+class FoodsController < ApplicationController
+  before_action :authenticate_user!
 
-#   def new
-#     @food = Food.new
-#     render layout: 'navbar_layout'
-#   end
+  def new
+    @food = Food.new
+    render layout: 'navbar_layout'
+  end
 
-#   # Add the create action
-#   def create
-#     @food = current_user.foods.build(food_params)
-#     # @food = Food.new(food_params)
+  # Add the create action
+  def create
+    @food = current_user.foods.build(food_params)
+    # @food = Food.new(food_params)
 
-#     if @food.save
-#       redirect_to recipes_path, notice: 'Food was successfully created.'
-#     else
-#       render 'new'
-#     end
-#   end
+    if @food.save
+      redirect_to recipes_path, notice: 'Food was successfully created.'
+    else
+      render 'new'
+    end
+  end
 
-#   def destroy
-#     @food = Food.find(params[:id])
-#     @food.destroy
-#     # redirect_to foods_path, notice: 'Food deleted successfully'
-#   end
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    # redirect_to foods_path, notice: 'Food deleted successfully'
+  end
 
-#   private
+  private
 
-#   def food_params
-#     params.require(:food).permit(:name, :measurement_unit, :price)
-#   end
-# end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
+end
